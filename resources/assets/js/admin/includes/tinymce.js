723cf9649104f56000773023feb10adb1dbf3ae7
Elliot Pettingale <elliot@kalexiko.com>
2018-11-17 17:41:59 +0000

    add code plugin to tinymce

--- resources/assets/js/admin/includes/tinymce.js
+++ resources/assets/js/admin/includes/tinymce.js
@@ -4,6 +4,7 @@ import image from 'tinymce/plugins/image';
 import link from 'tinymce/plugins/link';
 import advlist from 'tinymce/plugins/advlist';
 import lists from 'tinymce/plugins/lists';
+import code from 'tinymce/plugins/code';
 
 import 'tinymce/themes/modern/theme';
 
@@ -13,9 +14,9 @@ tinymce.init({
     menubar: false,
     height: 300,
     plugins: [
-        'advlist lists link image'
+        'advlist lists link image code'
     ],
-    toolbar1: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    toolbar1: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code",
     images_upload_handler: function(blobInfo, success, failure) {
         let textarea = $(`#${tinymce.activeEditor.id}`);
 
