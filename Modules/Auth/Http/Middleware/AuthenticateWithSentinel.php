eaceb3baacf2f34e0b0d5678512b12b2947eb69b
Elliot Pettingale <elipettingale@gmail.com>
2018-11-24 18:21:07 +0000

    if app environment is local automatically login developer instead of redirecting to login page

--- Modules/Auth/Http/Middleware/AuthenticateWithSentinel.php
+++ Modules/Auth/Http/Middleware/AuthenticateWithSentinel.php
@@ -4,23 +4,47 @@ namespace Modules\Auth\Http\Middleware;
 
 use Cartalyst\Sentinel\Sentinel;
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Modules\Auth\Repositories\UserRepository;
 
 class AuthenticateWithSentinel
 {
     private $sentinel;
+    private $userRepository;
 
-    public function __construct(Sentinel $sentinel) {
+    public function __construct(Sentinel $sentinel, UserRepository $userRepository)
+    {
         $this->sentinel = $sentinel;
+        $this->userRepository = $userRepository;
     }
 
     public function handle(Request $request, Closure $next)
     {
         if (!$this->sentinel->check()) {
-            session()->put('url.intended', $request->url());
-            return redirect()->route('admin.login.show');
+            if (env('APP_ENV') !== 'local') {
+                return $this->redirectToLogin($request->url());
+            }
+
+            $this->loginDeveloper();
         }
 
         return $next($request);
+    }
+
+    private function redirectToLogin(string $intended): RedirectResponse
+    {
+        session()->put('url.intended', $intended);
+
+        return redirect()->route('admin.login.show');
+    }
+
+    private function loginDeveloper(): void
+    {
+        $developer = $this->userRepository->findOrFail(
+            env('APP_DEVELOPER_ID', 1)
+        );
+
+        $this->sentinel->login($developer);
     }
 }
