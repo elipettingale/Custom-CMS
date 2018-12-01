fb3f72b8890bcd2fbf8127065dc11c981a6a3edb
Elliot Pettingale <elipettingale@gmail.com>
2018-12-01 09:07:24 +0000

    update trait to allow overwriting of diffForHumans options

--- Modules/Core/Traits/PresentsTimestamps.php
+++ Modules/Core/Traits/PresentsTimestamps.php
@@ -43,12 +43,16 @@ trait PresentsTimestamps
         return $this->entity->$attribute->format(CarbonFormats::DATE_TIME);
     }
 
-    public function diffForHumans(string $attribute)
+    public function diffForHumans(string $attribute, array $args = [])
     {
         if (!$this->entity->$attribute instanceof Carbon) {
             return null;
         }
 
-        return $this->entity->$attribute->diffForHumans();
+        $suffix = array_get($args, 'suffix', true);
+        $short = array_get($args, 'short', false);
+        $precision = array_get($args, 'precision', 1);
+
+        return $this->entity->$attribute->diffForHumans(null, !$suffix, $short, $precision);
     }
 }
